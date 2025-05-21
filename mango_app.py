622110c72b3f2fcd148e35363954e8ec9a19f078
MEYA-ESC <arnielynmea.escobal@msugensan.edu.ph>
2025-05-21 22:52:28 +0800

    Update mango_app.py

--- mango_app.py
+++ mango_app.py
@@ -81,8 +81,8 @@ zoom_pct = st.sidebar.slider("Crop Zoom (%)", 10, 40, 50)
 # --- Ripeness classification & analysis funcs ---
 def classify_ripeness_by_hue(hue):
     if   80 <= hue < 180:   return "Unripe"
-    elif 50 <= hue < 80:    return "Partially Ripe"
-    elif 40 <= hue < 50:    return "Ripe"
+    elif 60 <= hue < 80:    return "Partially Ripe"
+    elif 40 <= hue < 60:    return "Ripe"
     elif 0 <= hue < 40 or hue >= 330:
         return "Overripe"
     else:
